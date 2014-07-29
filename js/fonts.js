67e800a318409577661d336b7df1489cd399caff
prasanthabr <prasanthabr@gmail.com>
2014-07-29 09:52:18 +0530

    Initial

---
+++ js/fonts.js
@@ -0,0 +1,9 @@
+WebFont.load({
+  google: {
+    families: ['Droid Sans', 'Julius Sans One', 'Raleway', 'Roboto Condensed'],
+  },
+  custom: {
+    families: ['icomoonregular'],
+    urls: ['/font/stylesheet.css']
+  }
+});
---
+++ js/slide.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+	$('.mainslide').slick({
+	  accessibility: true,
+	  autoplay: true,
+	  arrows: false,
+	  centerMode: false,
+	  cssEase: 'ease',
+	  dots: true,
+	  fade: true,
+	  infinite: true,
+	  pauseOnHover: true,
+	  slide: 'div',
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  swipe: true,
+	  useCSS: true,
+	  autoplaySpeed: 10000
+	});
+});
